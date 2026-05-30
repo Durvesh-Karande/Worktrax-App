ae8c7715fc92f4f83359cf0b1c00e72dbeace2ec
Durvesh-Karande <durvesh@example.com>
2026-05-30 17:36:56 +0400

    Fix missing LinearLayout import in Workout_Logger_Logic

--- android/app/src/main/java/com/worktrax/app/ui/fragments/Workout_Logger_Logic.kt
+++ android/app/src/main/java/com/worktrax/app/ui/fragments/Workout_Logger_Logic.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
